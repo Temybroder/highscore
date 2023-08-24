92e2bb4b9127ab049529455401857eb5902732d2
Temitope Agboola <temybroder@gmail.com>
2023-08-24 18:07:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
-const fs = require("fs")
 const session = require('express-session');
-const sound = require("sound-play");
 const os = require('os');
 const path = require("path");
-// const fetch = ( ...args) => {
-//   import('node-fetch').then(({ default: fetch }) => fetch(...args));
-// }
-const sirenPath = path.join(__dirname, "/resources/siren.mp3");
-//sound.play(sirenPath);
 require("dotenv").config();
 const open = ( ...args) => {
   import('open').then(({ default: open }) => open(...args)); 
-} 
-// open('http://localhost:7000/spin', {app: {name: 'firefox'}});  
-
-
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function generateAndRegenerateRandomNumber() {
-  const randomNumber = getRandomInteger(1, 5);
-  console.log("Generated random number:", randomNumber);
-  
-  const interval = randomNumber * 60 * 1000; // Convert to milliseconds
-  setTimeout(generateAndRegenerateRandomNumber, interval);
-  
-  return randomNumber;
-}
-
-//generateAndRegenerateRandomNumber();
-
-
+}   
 
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
@@ -47,35 +20,6 @@ let testRun = require("./test/index")
 
 var starter = {}
 starter.init = () => {
-// testRun._app.runTests();
-
-const virtualization = require('./sdk/Virtualization');
-console.log(virtualization.initiatePlatform("youtube", "https://www.youtube.com/watch?v=KZ3tIGHU314"))
-
-
-// const startAllCreatedVDServers = require('./sdk/newVDSIndex')
-
-// startAllCreatedVDServers.spinVDServers("youtube", "https://www.youtube.com/watch?v=KZ3tIGHU314", 4)
-
-//"youtube", "https://www.youtube.com/watch?v=KZ3tIGHU314", 5;
-
-// TEST CODE
-// Function to check if server files have been created
-const testCheckServerFilesCreated = (num_of_servers) => {
-  fs.access("./createdServers/app1.js", fs.constants.F_OK, function (err){
-    if (err){
-      console.log("Server files check failed ===> Created Server directory is empty")
-      } else {
-          for(let i=0; i < num_of_servers; i++){
-              console.log("Server write function test passed")
-          }
-          // let serverEngine = require("")
-      }
-  })
-}
-
-testCheckServerFilesCreated(3);
-
 
 // EJS
 app.use(expressLayouts);
@@ -83,16 +27,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.set('views', path.join(__dirname, './public/views'));
 
-// To test route in app.js
-// app.get("/check", (req, res, next) => {
-// 	//res.send("Welcome to index")
-//   res.render("spinreport")
-// })
-
 // MASTER DATABASE CONNECTION
 const dbMain = require('./vars/db').mongoURI
-  
-// DB Config
 
 // Connect to MongoDB
 mongoose
@@ -119,15 +55,9 @@ app.use(
 // Passport Config
 require('./config/passport')(passport);
 
-// KEYS for OAUTH 2 integration 
-GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-GOOGLE_CLIENT_SECRET =  process.env.GOOGLE_CLIENT_SECRET;
-
-
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
  
-
 // // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -142,13 +72,6 @@ app.use(function(req, res, next) {
   res.locals.error = req.flash('error');
   next();
 });
-
-
-app.get('/testw', (req, res) => { 
-
-  res.send("Index route on this VD Server"); 
-
-}); 
 
 // Routes
 app.use('/', require('./routes/index'));
