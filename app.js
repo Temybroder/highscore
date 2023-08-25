9da7902123f9632993a6669a2e475a70112c6b07
Temitope Agboola <temybroder@gmail.com>
2023-08-25 01:29:07 +0100

    Update app.js ed app.js

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ const flash = require('connect-flash');
 
 
 
+
+
+// // Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+
+
 // Routes
 app.use('/', require('./routes/index'));
 
